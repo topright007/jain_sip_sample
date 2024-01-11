be9d5f25c196166405a1085a11d52285a0b0efb2
topright007 <elstano@gmail.com>
2024-01-11 23:57:22 +0300

    add jitsi dependencies

---
+++ src/main/kotlin/com/topright007/voicemenusip/VoicemenusipApplication.kt
@@ -0,0 +1,11 @@
+package com.topright007.voicemenusip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VoicemenusipApplication
+
+fun main(args: Array<String>) {
+	runApplication<VoicemenusipApplication>(*args)
+}
---
+++ src/test/kotlin/com/topright007/voicemenusip/VoicemenusipApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.topright007.voicemenusip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VoicemenusipApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
